5341c88db17c32f0f77046189349d7653c4e66db
hlibb <hlibastashev@gmail.com>
2023-01-05 19:21:15 +0100

    creating dictionary

--- src/main/java/com/DGcreate/Main.java
+++ src/main/java/com/DGcreate/Main.java
@@ -1,7 +1,19 @@
 package com.DGcreate;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Project");
+        // creating dictionary
+        String strDict = "note convince assist fetch reflect expose idea account churn hello wing slab";
+        List<String> stringsSorted = Arrays.stream(strDict.split(" ")).sorted().toList();
+        System.out.println(stringsSorted);
+
+        // hashset sorts on its own way
+        HashSet<String> dict = new HashSet<>(stringsSorted);
+        System.out.println(dict);
+        // 1 2 3 4 == 2 1 3 4 == 2 3 1 4 == 2 3 4 1
     }
 }
